70034c7f496772a71bcb883e91244479a43b815d
Nithin S J <nithinjayanjasla@gmail.com>
2024-06-02 18:36:26 +0530

    code update

--- Frontend/config.js
+++ Frontend/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL ="https://doctor-app-0sjm.onrender.com"
+export const BASE_URL ="https://doctor-app-0sjm.onrender.com/api/v1"
 export const token = localStorage.getItem('token')
 
