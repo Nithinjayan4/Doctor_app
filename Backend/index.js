d1a835703fe5c9018ece817a0088abecb49213d4
Nithin S J <nithinjayanjasla@gmail.com>
2024-06-02 18:24:05 +0530

    code update

--- Backend/index.js
+++ Backend/index.js
@@ -1,98 +1,3 @@
-// import express from 'express';
-// import http from 'http';
-// import cookieParser from 'cookie-parser';
-// import cors from 'cors';
-// import connectDB from './database/connectDB.js';
-// import dotenv from 'dotenv';
-// import authRoute from './Routes/auth.js';
-// import userRoute from './Routes/user.js';
-// import doctorRoute from './Routes/doctor.js';
-// import adminRoute from './Routes/admin.js';
-// import bookingRoute from './Routes/booking.js';
-// import chatRoute from './Routes/chat.js';
-// import notificationRoute from './Routes/notification.js';
-// import walletRoute from './Routes/wallet.js';
-// import { Server } from 'socket.io';
-
-// import path from "path"
-// const currentWorkingDir = path.resolve();
-// const parentDir = path.dirname(currentWorkingDir)
-// const productionParendDir = path.dirname(parentDir)
-// console.log('currentworkingdir:',currentWorkingDir)
-// console.log('parendDir:',parentDir)
-// console.log('productiondir:',productionParendDir)
-
-// dotenv.config();
-
-// const app = express();
-// const port = process.env.PORT || 8000;
-
-// const server = http.createServer(app);
-
-// const io = new Server(server, {
-//   cors: {
-//     origin: ["https://doctor-llwpu5ew9-nithinjayan4s-projects.vercel.app"],
-//   },
-// });
-
-// const corsOptions = {
-//   origin: true,
-// };
-
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(cors(corsOptions));
-
-// app.use('/api/v1/auth', authRoute);
-// app.use('/api/v1/users', userRoute);
-// app.use('/api/v1/doctors', doctorRoute);
-// app.use('/api/v1/admin', adminRoute);
-// app.use('/api/v1/bookings', bookingRoute);
-// app.use('/api/v1/chat', chatRoute);
-// app.use('/api/v1/notification', notificationRoute);
-// app.use('/api/v1/wallet', walletRoute);
-
-// io.on("connection", (socket) => {
-
-//   console.log('A user connected');
-
-//   socket.on('join', (user) => {
-//     if (user && user._id) {
-//       socket.join(user._id);
-//     }
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log('A user disconnected');
-//   });
-// });
-
-// app.use((req, res, next) => {
-//   req.io = io;
-//   next();
-// });
-
-// const enviornment = "production"
-
-// if (enviornment === 'production') { 
-//     app.use(express.static(path.join(parentDir ,'/Frontend/dist')));
-  
-//     app.get('*', (req, res) =>
-//       res.sendFile(path.resolve(parentDir,'Frontend', 'dist', 'index.html'))
-//     );
- 
-//   } else {
-//     app.get('/', (req, res) => {
-//       res.send('API is running....');
-//     });
-//   }
-
-
-
-// server.listen(port, () => {
-//   connectDB();
-//   console.log('Server is running on port', port);
-// });
 import express from 'express';
 import http from 'http';
 import cookieParser from 'cookie-parser';
@@ -108,7 +13,14 @@ import chatRoute from './Routes/chat.js';
 import notificationRoute from './Routes/notification.js';
 import walletRoute from './Routes/wallet.js';
 import { Server } from 'socket.io';
-import path from 'path';
+
+import path from "path"
+const currentWorkingDir = path.resolve();
+const parentDir = path.dirname(currentWorkingDir)
+const productionParendDir = path.dirname(parentDir)
+console.log('currentworkingdir:',currentWorkingDir)
+console.log('parendDir:',parentDir)
+console.log('productiondir:',productionParendDir)
 
 dotenv.config();
 
@@ -119,18 +31,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['https://doctor-llwpu5ew9-nithinjayan4s-projects.vercel.app'],
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true,
+    origin: [" http://localhost:5173"],
   },
 });
 
 const corsOptions = {
-  origin: 'https://doctor-llwpu5ew9-nithinjayan4s-projects.vercel.app',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type'],
-  credentials: true,
+  origin: true,
 };
 
 app.use(express.json());
@@ -146,7 +52,8 @@ app.use('/api/v1/chat', chatRoute);
 app.use('/api/v1/notification', notificationRoute);
 app.use('/api/v1/wallet', walletRoute);
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
+
   console.log('A user connected');
 
   socket.on('join', (user) => {
@@ -165,19 +72,22 @@ app.use((req, res, next) => {
   next();
 });
 
-const enviornment = 'production';
+const enviornment = "production"
 
-if (enviornment === 'production') {
-  app.use(express.static(path.join(path.dirname(path.resolve()), 'Frontend', 'dist')));
+if (enviornment === 'production') { 
+    app.use(express.static(path.join(parentDir ,'/Frontend/dist')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(parentDir,'Frontend', 'dist', 'index.html'))
+    );
+ 
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  }
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(path.dirname(path.resolve()), 'Frontend', 'dist', 'index.html'));
-  });
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+
 
 server.listen(port, () => {
   connectDB();
